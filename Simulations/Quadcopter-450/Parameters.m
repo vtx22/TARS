686d36dce69e9ecb19473143a5d52fc0fb48dc30
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-12-25 20:29:04 +0100

    Added Quadcopter Simulation

---
+++ Simulations/Quadcopter-450/Parameters.m
@@ -0,0 +1,15 @@
+%% Environmental Constants
+gravity = 9.81;
+airDensity = 1.2041;
+
+%% Quadcopter Parameters
+% Mass [kg]
+quadcopterMass = 1.4;
+% Arm length to center [m]
+armLength = 0.2;
+% Diameter
+diameter = 0.3;
+% Drag Coefficient
+dragCoeff = 0.1;
+
+
